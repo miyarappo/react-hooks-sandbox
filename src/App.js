0588fd9ba1ff37a148255a843388dc753e11a8c8
Minoru Miyata <miyarappo@gmail.com>
2019-10-31 12:45:21 +0900

    Create layouut

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
 
-const App = props => {
-  const [ name, setName ] = useState(props.name)
-  const [ price, setPrice ] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+const App = () => {
 
   return (
-    <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice( prev => prev + 1 )}>+1</button>
-      <button onClick={reset}>rest</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
-    </>
-  );
-}
+    <div>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div>
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input id="formEventTitle" />
+        </div>
+        <div>
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea id="formEventBody" />
+        </div>
+        <button>イベントを作成する</button>
+        <button>イベントを削除する</button>
+      </form>
 
-App.defaultProps = {
-  name: '',
-  price: 1000
+      <h4>イベント一覧</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
