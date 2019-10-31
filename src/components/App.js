abf3da765ef35dd8858a3d277cd8dcd6bda0a8c2
Minoru Miyata <miyarappo@gmail.com>
2019-10-31 19:00:30 +0900

    Create operationLogs reducer

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ console.log(AppContext)
 
 const App = () => {
   const initialState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [ state, dispatch ] = useReducer(reducer, initialState)
 
